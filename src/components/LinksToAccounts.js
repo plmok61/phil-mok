5c27fd6a7cc28a283d640e7922828709aee95d21
Phil <plmok61@gmail.com>
2016-11-24 11:13:34 -0600

    adds links to github, linkedin, and twitter

--- src/components/LinksToAccounts.js
+++ src/components/LinksToAccounts.js
@@ -4,9 +4,9 @@ class LinksToAccounts extends React.Component {
   render () {
     return (
       <div>
-        <h3>Github</h3>
-        <h3>LinkedIn</h3>
-        <h3>Twitter</h3>
+        <h3><a href="https://github.com/plmok61" target="blank">Github</a></h3>
+        <h3><a href="https://www.linkedin.com/in/philiplmok" target="blank">LinkedIn</a></h3>
+        <h3><a href="https://twitter.com/Phil_Mok" target="blank">Twitter</a></h3>
       </div>
     )
   }
