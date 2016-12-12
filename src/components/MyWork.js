6787e6ba23e3c8545e75608f921bfb9a6cb64d51
Phil <plmok61@gmail.com>
2016-12-11 18:43:22 -0600

    gets github repo data

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class MyWork extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      gitHubRepos: false,
+    }
+  }
+
+  componentDidMount () {
+    axios.get('https://api.github.com/users/plmok61/repos')
+      .then((response) => {
+        console.log(response.data)
+        this.setState({
+          gitHubRepos: response.data
+        })
+      })
+      .catch((error) => {
+        console.log('error getting repos: ', error)
+      }) 
+  }
+
   render () {
     return (
       <div>
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -6,7 +6,7 @@ export default class Summary extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      gitHubData: false
+      gitHubData: false,
     }
   }
 
