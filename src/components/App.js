de96e6a08eca1058503ddbddf87f086d3b8433e3
Phil <plmok61@gmail.com>
2017-02-15 21:04:34 -0600

    adds airbnb eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
-import '../App.css'
-import Summary from './Summary'
-import LinksToAccounts from './LinksToAccounts'
-import About from './About'
-import Container from './Container'
-import MyWork from './MyWork'
-import NotFound from './NotFound'
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import '../App.css';
+import Summary from './Summary';
+import LinksToAccounts from './LinksToAccounts';
+import About from './About';
+import Container from './Container';
+import MyWork from './MyWork';
+import NotFound from './NotFound';
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className='appContainer'>
+      <div className="appContainer">
         <Router history={hashHistory}>
-          <Route path='/' component={Container}>
+          <Route path="/" component={Container}>
             <IndexRoute component={Summary} />
-            <Route path='/about' component={About} />
-            <Route path='/contact' component={LinksToAccounts} />
-            <Route path='/work' component={MyWork} />
-            <Route path='*' component={NotFound} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={LinksToAccounts} />
+            <Route path="/work" component={MyWork} />
+            <Route path="*" component={NotFound} />
           </Route>
         </Router>
       </div>
-    )
+    );
   }
 }
-
