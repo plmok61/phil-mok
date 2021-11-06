ce443110b73e8708bf7b10adece01159104f6cfd
Phil Mok <plmok61@gmail.com>
2021-11-06 13:45:35 -0500

    Version 1.0.0 (#4)
    * force redeploy
    
    * gol working
    
    * wip
    
    * gol complete
    
    * cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
 import '../App.css';
-import Navigation from './navigation/navigation.container';
-import Footer from './footer/footer.container.js';
-import Home from './home/home.container';
-import DrakeEquation from './drake-equation/drake-equation.container';
+import GameOfLife from '../gol/game-of-life';
+import initialGrid from '../gol/initialGrid.json';
 
-class App extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
-  render() {
-    return (
-      <div>
-        <Navigation />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/drake-equation" component={DrakeEquation} />
-        <Footer />
-      </div>
-    );
-  }
+const GOL = new GameOfLife({ gridSize: 100, initialGrid });
+
+function App() {
+  const canvasRef = useRef(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const canvasClick = (event) => {
+      const { clientX, clientY } = event;
+      const x = Math.round(clientX / Math.ceil(window.innerWidth / GOL.gridSize));
+      const y = Math.round(clientY / Math.ceil(window.innerHeight / GOL.gridSize));
+      GOL.editGrid(x, y);
+    };
+
+    if (canvas) {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      GOL.initGrid(canvasRef.current);
+      setTimeout(() => {
+        GOL.startGame();
+      }, 1000);
+
+      const setGOTrue = () => setGameOver(true);
+      GOL.on('gameOver', setGOTrue);
+
+      canvas.addEventListener('click', canvasClick);
+    }
+    return () => {
+      GOL.removeAllListeners();
+      if (canvas) canvas.removeEventListener('click', canvasClick);
+    };
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={canvasRef}>
+        <p>fallback</p>
+      </canvas>
+      {gameOver && (
+        <button
+          className="newGameButton"
+          type="button"
+          onClick={() => {
+            GOL.initGrid(canvasRef.current);
+            GOL.startGame();
+          }}
+        >
+          New Game
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default App;
